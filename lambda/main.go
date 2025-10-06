1e6dff845a0477cc4ef6dbb03092305066c13134
Fernando <ftm.fernando@gmail.com>
2025-10-05 22:21:46 -0300

    Create main.go

---
+++ lambda/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "context"
+    "github.com/aws/aws-lambda-go/lambda"
+)
+
+type Request struct {
+    Name string `json:"name"`
+}
+
+type Response struct {
+    Message string `json:"message"`
+}
+
+func handler(ctx context.Context, req Request) (Response, error) {
+    return Response{
+        Message: "Hello " + req.Name,
+    }, nil
+}
+
+func main() {
+    lambda.Start(handler)
+}
